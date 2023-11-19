87605bc87f3c4d364bc68231bfa398328b3547d9
Greg Norman Millora <gregmillora.dev@gmail.com>
2023-11-20 00:26:47 +0800

    NestJS&NextJS template init

---
+++ apps/web-app/app/page.tsx
@@ -0,0 +1,14 @@
+import { Input } from '@nx-template/web-app/ui-components';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+}
